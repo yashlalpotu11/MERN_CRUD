ba09ae109a1f87c5b26b800ed0ba9527ef096b0a
YASH  LALPOTU <yashrlalpotu30@gmail.com>
2021-11-07 22:57:54 +0530

    Working on Adding student data in Database

---
+++ client/src/Components/AddStudent/addStudent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './addStudent.css'
+import axios from 'axios'   
+import {toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+export default function addStudent() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
